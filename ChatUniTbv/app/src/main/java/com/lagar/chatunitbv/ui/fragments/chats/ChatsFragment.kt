fa3ec62861edb87b12007d35f73aeb66922257b3
Adrian Călăvie <adrian.calavie06@gmail.com>
2021-12-07 03:33:29 +0200

    Optimised change listener
    The RecyclerView now properly reacts to ADD, REMOVE, MODIFY events

--- ChatUniTbv/app/src/main/java/com/lagar/chatunitbv/ui/fragments/chats/ChatsFragment.kt
+++ ChatUniTbv/app/src/main/java/com/lagar/chatunitbv/ui/fragments/chats/ChatsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.toObject
 import com.lagar.chatunitbv.databinding.ChatsFragmentBinding
@@ -20,11 +21,11 @@ import timber.log.Timber
 
 class ChatsFragment : Fragment() {
 
+    private lateinit var fastAdapter: FastAdapter<ChatItem>
     private var _binding: ChatsFragmentBinding? = null
     private lateinit var itemAdapter: ItemAdapter<ChatItem>
 
     private val binding get() = _binding!!
-    private val items = ArrayList<ChatItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,18 +33,57 @@ class ChatsFragment : Fragment() {
         _binding = ChatsFragmentBinding.inflate(layoutInflater)
 
         itemAdapter = ItemAdapter()
-        val fastAdapter = FastAdapter.with(itemAdapter)
+        fastAdapter = FastAdapter.with(itemAdapter)
+        binding.chatsRv.adapter = fastAdapter
 
-//        createTempChats()
-
-        populateAdapter()
+        attachListener()
 
         val layoutManager = LinearLayoutManager(context)
 
         binding.chatsRv.also {
             it.layoutManager = layoutManager
-            it.adapter = fastAdapter
         }
+    }
+
+    private fun attachListener() {
+        Operations.database.collection("chats")
+            .orderBy("name", Query.Direction.DESCENDING)
+            .addSnapshotListener { chatsDocsChanged, e ->
+                if (e != null) {
+                    Timber.w("Chats listen error: $e")
+                    return@addSnapshotListener
+                }
+
+
+                for (chatDoc in chatsDocsChanged!!.documentChanges) {
+                    when (chatDoc.type) {
+                        DocumentChange.Type.ADDED -> {
+                            Timber.d("Chat document added: ${chatDoc.document.data}")
+                            itemAdapter.add(chatDoc.newIndex, ChatItem(chatDoc.document.toObject()))
+                        }
+                        DocumentChange.Type.REMOVED -> {
+                            Timber.d("Chat document removed: ${chatDoc.document.data}")
+                            itemAdapter.remove(chatDoc.oldIndex)
+                        }
+                        DocumentChange.Type.MODIFIED -> {
+                            Timber.d("Chat document modified: ${chatDoc.document.data}")
+                            if (chatDoc.newIndex != chatDoc.oldIndex) {
+
+                                itemAdapter.remove(chatDoc.oldIndex)
+
+                                itemAdapter.add(
+                                    chatDoc.newIndex,
+                                    ChatItem(chatDoc.document.toObject())
+                                )
+                            } else {
+                                itemAdapter[chatDoc.newIndex] =
+                                    ChatItem(chatDoc.document.toObject())
+                            }
+
+                        }
+                    }
+                }
+            }
     }
 
     override fun onCreateView(
@@ -51,37 +91,8 @@ class ChatsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        
         return binding.root
-    }
-
-    private fun createTempChats() {
-        for (i in 1..24) {
-            Operations.database.collection("chats")
-                .add(Chat("", "Chat#$i", "10:2${i % 10} AM"))
-                .addOnSuccessListener {
-                    Timber.d("DocumentSnapshot added with ID: ${it.id}")
-                }
-                .addOnFailureListener {
-                    Timber.w("Error adding document. Error: $it")
-                }
-        }
-    }
-
-    private fun populateAdapter() {
-        Operations.database.collection("chats")
-            .orderBy("name", Query.Direction.DESCENDING)
-            .addSnapshotListener { chatsDocs, e ->
-                if (e != null) {
-                    Timber.w("Listen failed: $e")
-                    return@addSnapshotListener
-                }
-                items.clear()
-                for (chat in chatsDocs!!) {
-                    items.add(ChatItem(chat.toObject()))
-                }
-                itemAdapter.set(items)
-            }
     }
 
     override fun onDestroyView() {
